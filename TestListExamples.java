2597777b77b8bcc31b27ef7f1f7a13a867181353
sylvanabrooke <114433539+sylvanabrooke@users.noreply.github.com>
2022-11-10 12:26:56 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -3,4 +3,23 @@ import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test 
+	public void testFilter() {
+        List<String> temp = new ArrayList<>();
+        StringChecker s = new StringChecker(){
+            public boolean checkString(String s) {
+                if(s.length()<4){
+                  return true;
+                }
+                return false;
+            }
+        };
+        temp.add("one");
+        temp.add("two");
+        temp.add("three");
+        List<String> answer = new ArrayList<String>();
+        answer.add("one");
+        answer.add("two");
+        assertEquals(answer, ListExamples.filter(temp, s));
+	}
 }
