bcf0c975583f219984ad3b233f87e453302db62e
sylvanabrooke <114433539+sylvanabrooke@users.noreply.github.com>
2022-11-10 13:48:07 -0800

    updated

--- grade.sh
+++ grade.sh
@@ -9,8 +9,23 @@ cd student-submission
 if test -f ListExamples.java
 then 
     echo "ListExamples.jav exists"
-    exit 0
 else 
     echo "ListExamples.java does not existtttttttt"
     exit 1
 fi
+
+cp ListExamples.java ../
+
+cd ..
+javac -cp ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" TestListExamples.java ListExamples.java
+if test $? -eq 0;
+then
+    echo "compile sucess!!!"
+else 
+    echo "quit CS!! you're so pretty"
+    exit 1
+fi
+
+set +e 
+
+java -cp ".:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
